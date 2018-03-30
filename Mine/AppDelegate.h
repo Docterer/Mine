a70639093c74b4bbac9169bddcbe9bbe757371b6
docterer <daladidali@gmail.com>
2018-03-30 14:11:23 +0800

    初始提交

---
+++ Mine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mine
+//
+//  Created by 单怡然 on 2018/3/30.
+//  Copyright © 2018年 单怡然. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mine
+//
+//  Created by 单怡然 on 2018/3/30.
+//  Copyright © 2018年 单怡然. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
